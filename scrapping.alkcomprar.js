d3495ada8ecfbbaa0adc6066cebf1f71a9290636
daniel72584@gmail.com <daniel72584@gmail.com>
2020-06-22 10:32:01 -0500

    scraping de alcomprar

--- scrapping.alkcomprar.js
+++ scrapping.alkcomprar.js
@@ -8,9 +8,10 @@ async function cheerioExample() {
     const encotnrados = $('li.product__list--item').map((_, el) => {
         el = $(el); 
         const title = el.find('div.product__information').find('h2.product__information--name').text();
-        const description = el.find('div.price__container').text();
-        const link = el.find('div.images-viewer').attr('item-url');
-        const image = el.find('div.product__image').find('div.image-content').find('a').find('img').attr('data-src');
+        const description = el.find('div.product__price').find('span.price').text();
+        const link = 'https://www.alkomprar.com/'+el.find('div.product__image').find('a.js-product-click-datalayer').attr('href');
+        const image = 'https://www.alkomprar.com/'+ el.find('div.product__image').find('a.js-product-click-datalayer').find('div.product__image__container').find('img').attr('data-src');
+        //.find('img').attr('src');
         return { title, description, link,image };
     }).get()
     return encotnrados;
