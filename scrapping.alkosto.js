24a3ce95e62ccbbde57f9d4ee510f3c029107e15
daniel72584@gmail.com <daniel72584@gmail.com>
2020-06-21 21:37:35 -0500

    Scrapping de alkost y falabela listos, falta exito y mercado libre

---
+++ scrapping.alkosto.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const scrapeIt = require("scrape-it");
+
+async function cheerioExample() {
+    const pageContent = await axios.get('https://www.alkosto.com/salesperson/result/?q=go+pro+hero+7');
+    const $ = cheerio.load(pageContent.data,{
+        normalizeWhitespace: true,
+    });
+    const presentations = $('.salesperson-products-grid-item').map((_, el) => {
+        el = $(el); 
+        //console.log(el)
+        const title = el.find('h2.product-name').text();
+        const description = el.find('.price').find('.price').text();
+        const link = el.find('a.product-image').attr('href');
+        const image = el.find('a.product-image').find('img').attr('src');
+        //console.log(title,description,link,image)
+        return { title, description, link,image };
+    }).get();
+    console.log(presentations);
+}
+
+
+cheerioExample().then((data)=>{
+
+}).catch(error=>{
+    console.log('error',error.message,error.stack);
+})
