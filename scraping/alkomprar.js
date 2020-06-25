81063c672ab82aea207149afeba64f6ad8c2ecda
daniel ortiz <dortiz@resolvestudio.co>
2020-06-24 20:21:39 -0500

    Se cambia todo

--- scraping/alkomprar.js
+++ scraping/alkomprar.js
@@ -1,21 +1,29 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const scrapeIt = require("scrape-it");
 
-module.exports.scrapping=async function scrapping(search,responses) {
-    search = search.replace(' ','+')
-    const pageContent = await axios.get('https://www.alkomprar.com/search/?text='+search);
+module.exports.scrapping = async function scrapping(search, responses) {
+    search = search.replace(' ', '+')
+    const pageContent = await axios.get('https://www.alkomprar.com/search/?text=' + search);
     const $ = cheerio.load(pageContent.data);
     const encotnrados = $('li.product__list--item').map((_, el) => {
-        el = $(el); 
+        el = $(el);
         const title = el.find('div.product__information').find('h2.product__information--name').text().trim();
+
+        search.split(' ').forEach(text => {
+            console.log('compara', search, text)
+            if (!title.includes(text)) {
+                return 'false'
+            }
+        })
+
+
         const description = el.find('div.product__price').find('span.price').text().trim();
-        const link = 'https://www.alkomprar.com/'+el.find('div.product__image').find('a.js-product-click-datalayer').attr('href');
-        const image = 'https://www.alkomprar.com/'+ el.find('div.product__image').find('a.js-product-click-datalayer').find('div.product__image__container').find('img').attr('data-src');
+        const link = 'https://www.alkomprar.com/' + el.find('div.product__image').find('a.js-product-click-datalayer').attr('href');
+        const image = 'https://www.alkomprar.com/' + el.find('div.product__image').find('a.js-product-click-datalayer').find('div.product__image__container').find('img').attr('data-src');
         //.find('img').attr('src');
-        responses.push({ title, description, link,image })
-        return { title, description, link,image };
+        responses.push({ title, description, link, image, fuente: 'alkomprar' })
+        return { title, description, link, image };
     }).get()
     return encotnrados;
-   
+
 }
--- scraping/mercadolibre.js
+++ scraping/mercadolibre.js
@@ -1,19 +1,19 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-module.exports.scrapping=async function scrapping(search,responses) {
-    search=search.replace(' ','-')
-    search='https://listado.mercadolibre.com.co/'+search
+module.exports.scrapping = async function scrapping(search, responses) {
+    search = search.replace(' ', '-')
+    search = 'https://listado.mercadolibre.com.co/' + search
     const pageContent = await axios.get(search);
     const $ = cheerio.load(pageContent.data);
     const encotnrados = $('li.results-item').map((_, el) => {
-        el = $(el); 
+        el = $(el);
         const title = el.find('div.item__info-container').find('h2.list-view-item-title').text().trim();
         const description = el.find('div.price__container').text().trim();
         const link = el.find('div.images-viewer').attr('item-url');
         const image = el.find('div.images-viewer').find('div.image-content').find('a').find('img').attr('data-src');
-        responses.push({ title, description, link,image })
-        return { title, description, link,image };
+        responses.push({ title, description, link, image })
+        return { title, description, link, image, fuente: 'mercadolibre' };
     }).get()
     return encotnrados;
 }
