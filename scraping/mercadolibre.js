d3a1552f26e5eb058064dda74db408d2707a2465
daniel ortiz <dortiz@resolvestudio.co>
2020-07-03 18:02:07 -0500

    Cambios menores

--- scraping/mercadolibre.js
+++ scraping/mercadolibre.js
@@ -1,12 +1,15 @@
 const axios = require('axios');
+
+
+
 const cheerio = require('cheerio');
 const filters = require('../scraping/filters');
 
-module.exports.scrappingByWeb = async function scrapping(search, responses, size,keywords) {
+module.exports.scrappingByWeb = async function scrapping(search, responses, size, keywords) {
     try {
-    const pageContent = await axios.get(search);
-    const $ = cheerio.load(pageContent.data);
-    let i = 0
+        const pageContent = await axios.get(search);
+        const $ = cheerio.load(pageContent.data);
+        let i = 0
 
         const encotnrados = $('li.results-item').map((_, el) => {
             console.log('carga')
@@ -15,8 +18,8 @@ module.exports.scrappingByWeb = async function scrapping(search, responses, size
             }
             el = $(el);
             const title = el.find('div.item__info-container').find('h2.list-view-item-title').text().trim();
-            
-            if(!filters.filterByKeyWords(title,keywords)) return;
+
+            if (!filters.filterByKeyWords(title, keywords)) return;
 
             const description = el.find('div.price__container').text().trim();
             const link = el.find('div.images-viewer').attr('item-url');
@@ -39,12 +42,12 @@ module.exports.scrappingByWeb = async function scrapping(search, responses, size
 
 module.exports.scrapping = async function scrapping(search, responses, size) {
     try {
-    search = search.normalize().replace(' ', '-')
-    search = 'https://listado.mercadolibre.com.co/' + search
-    console.log('bsqueda',search)
-    const pageContent = await axios.get(search);
-    const $ = cheerio.load(pageContent.data);
-    let i = 0
+        search = search.normalize().replace(' ', '-')
+        search = 'https://listado.mercadolibre.com.co/' + search
+        console.log('bsqueda', search)
+        const pageContent = await axios.get(search);
+        const $ = cheerio.load(pageContent.data);
+        let i = 0
 
         const encotnrados = $('li.results-item').map((_, el) => {
             if (i >= size) {
@@ -53,7 +56,7 @@ module.exports.scrapping = async function scrapping(search, responses, size) {
             el = $(el);
             const title = el.find('div.item__info-container').find('h2.list-view-item-title').text().trim();
 
-            if(!filters.filterByName(title,search)) return;
+            if (!filters.filterByName(title, search)) return;
 
             const description = el.find('div.price__container').text().trim();
             const link = el.find('div.images-viewer').attr('item-url');
